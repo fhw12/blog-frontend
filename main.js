c45887bac847d2c96dd392949a6a140b8d3cd5b4
fhw12 <nikita.pymitron@gmail.com>
2024-07-30 08:08:29 +0700

    pages classes and AppController class

--- main.js
+++ main.js
@@ -1,58 +1,106 @@
 'use strict';
 
-const app = document.querySelector('#app');
-const websiteNameElement = document.querySelector('#website-name');
+class MainPageController{
+    constructor(appController){
+        this.appController = appController;
+        this.open();
+    }
 
-websiteNameElement.addEventListener('click', openMainPage);
+    open(){
+        this.appController.page.innerHTML = '<div class="post-container" id="post-container"></div>';
+        for(let i = 0; i < 30; i++){
+            this.addPost(
+                'Programming - 29 July, 2024',
+                'How to create a website?',
+                'In this post you will learn of how to create your own website by using html, css, js'
+            );
+        }
+    }
 
-function addPost(postContainer, info, title, description){
-    postContainer.innerHTML += `
-        <div class="post">
-            <p class="post-theme-date">${info}</p>
-            <p class="post-title">${title}</p>
-            <p class="post-description">${description}</p>
-        </div>
-    `;
-}
-
-function openMainPage(){
-    app.innerHTML = '<div class="post-container" id="post-container"></div>';
-    const postContainer = document.querySelector('#post-container');
-    for(let i = 0; i < 3; i++){
-        addPost(postContainer,
-            'Programming - 29 July, 2024',
-            'How to create a website?',
-            'In this post you will learn of how to create your own website by using html, css, js'
-        );
+    addPost(info, title, description){
+        const postContainer = document.querySelector('#post-container');
+        postContainer.innerHTML += `
+            <div class="post">
+                <p class="post-theme-date">${info}</p>
+                <p class="post-title">${title}</p>
+                <p class="post-description">${description}</p>
+            </div>
+        `;
     }
 }
 
-function openSignUpPage(){
-    app.innerHTML = `
-<div>
-    <h2>Create an account</h2>
-    <p class="field-label">Your login:</p>
-    <input class="input-field" placeholder="Login" id="login">
-    <p class="field-label">Your password:</p>
-    <input class="input-field" placeholder="Password" id="password">
-    <p class="field-label">Repeat your password:</p>
-    <input class="input-field" placeholder="Repeat the password" id="repeatedPassword">
-    <button class="form-button">Create a new account</button>
-</div>
-    `;
+class SignInPageController{
+    constructor(appController){
+        this.appController = appController;
+        this.open();
+    }
+
+    open(){
+        this.appController.page.innerHTML = `
+            <div>
+                <h2>Log in</h2>
+                <p class="field-label">Your login:</p>
+                <input class="input-field" placeholder="Login" id="login">
+                <p class="field-label">Your password:</p>
+                <input class="input-field" placeholder="Password" id="password">
+                <button class="form-button">Login</button>
+            </div>
+        `;
+    }
 }
 
-function openSignInPage(){
-    app.innerHTML = `
-    <div>
-<h2>Log in</h2>
-    <p class="field-label">Your login:</p>
-    <input class="input-field" placeholder="Login" id="login">
-    <p class="field-label">Your password:</p>
-    <input class="input-field" placeholder="Password" id="password">
-    <button class="form-button">Login</button>
-</div>
-    `;
+class SignUpPageController{
+    constructor(appController){
+        this.appController = appController;
+        this.open();
+    }
+
+    open(){
+        this.appController.page.innerHTML = `
+            <div>
+                <h2>Create an account</h2>
+                <p class="field-label">Your login:</p>
+                <input class="input-field" placeholder="Login" id="login">
+                <p class="field-label">Your password:</p>
+                <input class="input-field" placeholder="Password" id="password">
+                <p class="field-label">Repeat your password:</p>
+                <input class="input-field" placeholder="Repeat the password" id="repeatedPassword">
+                <button class="form-button">Create a new account</button>
+            </div>
+        `;
+    }
 }
 
-openMainPage();
+class AppController{
+    constructor(){
+        this.page = document.querySelector('#page');
+
+        this.createEvents();
+        this.openMainPage();
+    }
+
+    createEvents(){
+        const websiteNameElement = document.querySelector('#website-name');
+        websiteNameElement.addEventListener('click', this.openMainPage.bind(this));
+
+        const signInButton = document.querySelector('#signin');
+        const signUpButton = document.querySelector('#signup');
+
+        signInButton.addEventListener('click', this.openSignInPage.bind(this));
+        signUpButton.addEventListener('click', this.openSignUpPage.bind(this));
+    }
+
+    openMainPage(){
+        new MainPageController(this);
+    }
+
+    openSignUpPage(){
+        new SignUpPageController(this);
+    }
+
+    openSignInPage(){
+        new SignInPageController(this);
+    }
+}
+
+new AppController();
