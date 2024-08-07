2db64c8c0f676de59c3b5a5c552b371d246a5944
fhw12 <nikita.pymitron@gmail.com>
2024-08-07 19:41:54 +0700

    logIn request

--- classes/api.js
+++ classes/api.js
@@ -4,22 +4,47 @@ class APIRequests{
         this.#baseURL = "http://localhost:8000"; // server address
     }
 
+    async responseParsing(response){
+        if(response?.ok){
+            try{
+                const result = await response.json();
+                return result;
+            } catch(err){
+                console.log('Response json-parsing error | ' + err);
+                return null;
+            }
+        } else {
+            console.log('Response status | ' + response?.status);
+            return null;
+        }
+    }
+
     async getPosts(){
         try{
             const response = await fetch(`${this.#baseURL}/posts`);
+            return await this.responseParsing(response);
+        } catch(err){
+            console.log('Response error | ' + err);
+            return null;
+        }
+    }
 
-            if(response?.ok){
-                try{
-                    const result = await response.json();
-                    return result;
-                } catch(err){
-                    console.log('Response json-parsing error | ' + err);
-                    return null;
+    async signIn(login, password){
+        try{
+            const response = await fetch(
+                `${this.#baseURL}/signin`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        login: login,
+                        password: password
+                    }),
                 }
-            } else {
-                console.log('Response status | ' + response?.status);
-                return null;
-            }
+            );
+            return await this.responseParsing(response);
         } catch(err){
             console.log('Response error | ' + err);
             return null;
