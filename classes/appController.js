dcd9d6cfd9be13ff051dde24d3800eff76529289
fhw12 <nikita.pymitron@gmail.com>
2024-08-11 12:50:07 +0700

    check if the user logged in

--- classes/appController.js
+++ classes/appController.js
@@ -2,9 +2,41 @@ class AppController{
     constructor(){
         this.page = document.querySelector('#page');
         this.repository = new Repository();
+        this.api = new APIRequests();
 
         this.createEvents();
+        this.authUser();
         this.openHomePage();
+    }
+
+    authUser(){
+        const token = this.repository.getCookie('token');
+        this.api.getCurrentUser(token).then(
+            (result) => {
+                console.log(result);
+
+                if(result){
+                    const headerRightElement = document.querySelector('.header-right');
+
+                    const profileButton = document.createElement('button');
+                    profileButton.className = 'button margin-right';
+                    profileButton.textContent = `Profile(${result.username})`;
+
+                    const logoutButton = document.createElement('button');
+                    logoutButton.className = 'button';
+                    logoutButton.textContent = 'Logout';
+                    logoutButton.addEventListener('click', this.logout);
+
+                    headerRightElement.innerHTML = '';
+                    headerRightElement.appendChild(profileButton);
+                    headerRightElement.appendChild(logoutButton);
+                }
+            }
+        );
+    }
+
+    logout(){
+        document.cookie = "token=; max-age=-1; path=/;";
     }
 
     createEvents(){
